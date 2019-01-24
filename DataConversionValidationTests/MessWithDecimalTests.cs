5b4f807d232d9abbdff85309b8e95fd35432875c
Haq, Reaz (CAI - Austin) <Reaz.Haq@coxautoinc.com>
2019-01-24 14:59:20 -0600

    more unit tests

--- DataConversionValidationTests/MessWithDecimalTests.cs
+++ DataConversionValidationTests/MessWithDecimalTests.cs
@@ -15,16 +15,20 @@ namespace DataConversionValidationTests
         }
 
         [Theory]
-        [InlineData(0.00, true)]
-        [InlineData(3.333, false)]
-        [InlineData(5.5, true)]
-        [InlineData(7, true)]
-        [InlineData(0.0002, false)]
-        public void CheckForTwoDecimalPlaces(decimal d, bool expectedResult)
+        [InlineData(0.00, 2, true)]
+        [InlineData(3.333, 2, false)]
+        [InlineData(3.333, 3, true)]
+        [InlineData(5.5, 2, true)]
+        [InlineData(5.5, 1, true)]
+        [InlineData(5.5, 0, false)]
+        [InlineData(5.2, 0, false)]
+        [InlineData(7, 2, true)]
+        [InlineData(0.0002, 2, false)]
+        public void CheckForTwoDecimalPlaces(decimal d, int decimalPlacesToCheck, bool expectedResult)
         {
             _testOutputHelper.WriteLine($"{d}");
-            _testOutputHelper.WriteLine($"{decimal.Round(d, 2)}");
-            var result = MessWithDecimal.CheckDecimalToXDecimalPlaces(d, 2);
+            _testOutputHelper.WriteLine($"{decimal.Round(d, decimalPlacesToCheck)}");
+            var result = MessWithDecimal.CheckDecimalToXDecimalPlaces(d, decimalPlacesToCheck);
             _testOutputHelper.WriteLine($"{d} is {result}");
 
             Assert.Equal(expectedResult, result);
