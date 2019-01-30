a941f8025a6bcee5a1258b00cd33a33ddf62ff26
Haq, Reaz (CAI - Austin) <Reaz.Haq@coxautoinc.com>
2019-01-30 15:35:01 -0600

    gc collection call completeness

--- GcExperiments/PrematureGcCleanup471/Program.cs
+++ GcExperiments/PrematureGcCleanup471/Program.cs
@@ -9,7 +9,12 @@ namespace PrematureGcCleanup471
         {
             var timer = new Timer(MyTimerCallback, null, 2000, 500);
             Console.WriteLine("timer started - hit enter to stop");
-            //GC.Collect(); // if this line enabled... only debug works
+            
+            GC.Collect(); // if this line enabled... only debug works
+            // these two are for force gc collection completeness
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
             //GC.KeepAlive(timer); // to fix 
             Console.ReadLine();
         }
--- GcExperiments/PrematureGcCleanupNetCore/Program.cs
+++ GcExperiments/PrematureGcCleanupNetCore/Program.cs
@@ -9,7 +9,12 @@ namespace PrematureGcCleanupNetCore
         {
             var timer = new Timer(MyTimerCallback, null, 2000, 500);
             Console.WriteLine("timer started - hit enter to stop");
+            
             GC.Collect(); // if this line enabled... only debug works
+            // these two are for force gc collection completeness
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
             GC.KeepAlive(timer); // to fix 
             Console.ReadLine();
         }
