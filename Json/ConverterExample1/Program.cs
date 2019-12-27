fd5fc5dad44e26e86aaaaee2d79cd70697cf5df4
reazhaq <reaz.haq@outlook.com>
2019-12-27 09:56:53 -0600

    json converter factory example

---
+++ Json/ConverterExample1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace SystemTextJsonSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializeOptions = new JsonSerializerOptions();
+            serializeOptions.Converters.Add(new DictionaryTKeyEnumTValueConverter());
+
+            var o = new WeatherForecastWithEnumDictionary
+            {
+                Date = DateTimeOffset.Now,
+                Summary = "summary",
+                TemperatureCelsius = 15,
+                TemperatureRanges = new Dictionary<SummaryWordsEnum, int>
+                {
+                    {SummaryWordsEnum.Cold, 0},
+                    {SummaryWordsEnum.Hot, 100}
+                }
+            };
+
+            var serializedObject = JsonSerializer.Serialize(o, serializeOptions);
+            Debug.WriteLine($"{serializedObject}");
+
+            var o2 = JsonSerializer.Deserialize<WeatherForecastWithEnumDictionary>(serializedObject, serializeOptions);
+            Debug.WriteLine($"{o2.TemperatureCelsius}");
+        }
+    }
+}
---
+++ Json/ConverterExample1/WeatherForecastWithEnumDictionary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SystemTextJsonSamples
+{
+    public class WeatherForecastWithEnumDictionary
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+        public Dictionary<SummaryWordsEnum, int> TemperatureRanges { get; set; }
+    }
+
+    public enum SummaryWordsEnum
+    {
+        Cold, Hot
+    }
+}
